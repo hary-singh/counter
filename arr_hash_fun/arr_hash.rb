88c033ecdd60ecc6cad334db31e0cfaf948e651f
Harry Dhillon <navbirsdhillon@gmail.com>
2021-02-18 19:28:03 -0700

    day 2

---
+++ arr_hash_fun/arr_hash.rb
@@ -0,0 +1,28 @@
+# a.each do | num |
+#     puts num
+# end
+
+# a << 9
+# a.each_with_index do | num1 , i |
+#     puts " #{i+1}. #{num1}"
+# end
+
+# a.unshift(4)
+# a.insert(2, 50)
+# a.each_with_index do | num1 , i |
+#     puts " #{i+1}. #{num1}"
+# end
+
+# a = [5,6,7,8,50]
+# # p a.sort
+# p a.rotate
+
+# person = { 1 => "bob", 2 => "smith"}
+
+# person1 = [{first_name: "bib", last_name: "smith", age: 30}, {first_name: "jake", last_name: "smith", age: 20}]
+
+# p person1
+
+# person1.each do |keys, values|
+#     p "#{keys}"
+# end
---
+++ ruby_day1/day1.rb
@@ -0,0 +1,18 @@
+# puts 1 + '25'.to_i
+
+Foo = 2
+@foo = 3
+
+foo = 1
+
+
+def run_bar(foo)
+
+    # puts Foo
+    # puts @foo
+    foo * 2
+
+end
+
+res = run_bar(foo)
+puts res
