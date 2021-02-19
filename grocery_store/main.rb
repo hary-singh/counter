f2d36595fe4b4dec10c285950403e3b3800f20ad
Harry Dhillon <navbirsdhillon@gmail.com>
2021-02-18 20:52:03 -0700

    updates

--- grocery_store/main.rb
+++ grocery_store/main.rb
@@ -50,16 +50,19 @@ def item_menu_choice
     item_choice = gets.chomp.to_i
     if item_choice === 1
       puts "*** 1 Apple added to Cart ***"
+      Price + = Items[item_choice-1][:cost]
       items_show
     elsif item_choice === 2
       puts "*** 1 Orange added to Cart ***"
+      Price + = Items[item_choice-1][:cost]
       items_show
     elsif item_choice === 3
         puts "*** 1 Banana added to Cart ***"
+        Price + = Items[item_choice-1][:cost]
         items_show
     elsif item_choice === 4
       puts "** Going back to Main Menu **"
-      main_menu_choice
+      menu
     else
         puts "not valid choice, try again"
         item_menu_choice
